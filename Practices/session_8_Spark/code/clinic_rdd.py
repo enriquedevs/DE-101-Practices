ecf7cf335a39d1ba4206d7a1a3fed09aa52a16da
Enrique Garcia <nitos_san@hotmail.com>
2023-03-09 20:56:59 -0600

    Adding clinic rdd example

--- Practices/session_8_Spark/code/clinic_rdd.py
+++ Practices/session_8_Spark/code/clinic_rdd.py
@@ -1,6 +1,4 @@
 from pyspark import SparkConf, SparkContext
-import psycopg2
-import csv
 
 # PostgreSQL connection parameters
 conn_params = {
@@ -11,66 +9,55 @@ conn_params = {
     "password": "mypassword"
 }
 
+# Define a function to return all elements except for the first one
+def remove_first(iterator):
+    return list(iterator)[1:]
+
+# Define save functions
+def save_to_postgres(df, table_name):
+    df.write \
+        .format("jdbc") \
+        .option("url", f"jdbc:postgresql://{conn_params['host']}:{conn_params['port']}/{conn_params['database']}") \
+        .option("dbtable", table_name) \
+        .option("user", conn_params["user"]) \
+        .option("password", conn_params["password"]) \
+        .option("driver", "org.postgresql.Driver") \
+        .mode("append") \
+        .save()
+
 # Initialize Spark
 conf = SparkConf().setAppName("ClinicApp").setMaster("local[*]")
 sc = SparkContext(conf=conf)
 
-# Read CSV file into RDD
-csv_file = sc.textFile("./clinic_1.csv")
-header = csv_file.first()
-data = csv_file.filter(lambda row: row != header)
+# Read CSV to RDD
+file_rdd = sc.textFile('./clinic_1.csv')
 
-# Define transformation functions
-def split_row(row):
-    fields = row.split(",")
-    return (fields[0], fields[1], fields[2], fields[3], fields[4], fields[5], fields[6], fields[7])
+print(f'file has {file_rdd.count()} rows')
 
-def save_patient(row):
-    conn = psycopg2.connect(**conn_params)
-    cur = conn.cursor()
-    cur.execute("INSERT INTO patient (name, last_name, address) VALUES (%s, %s, %s) RETURNING id", (row[0], row[1], row[2]))
-    patient_id = cur.fetchone()[0]
-    conn.commit()
-    cur.close()
-    conn.close()
-    return (patient_id, row)
+print('CSV file content')
+file_rdd.foreach(lambda row: print(row))
 
-def save_clinical_specialization(row):
-    conn = psycopg2.connect(**conn_params)
-    cur = conn.cursor()
-    cur.execute("INSERT INTO clinical_specialization (name) VALUES (%s) RETURNING id", (row[7],))
-    clinical_specialization_id = cur.fetchone()[0]
-    conn.commit()
-    cur.close()
-    conn.close()
-    return (clinical_specialization_id, row)
+# Obtaining CSV file content without the header
+rows_rdd = file_rdd.mapPartitionsWithIndex(lambda i, iterator: remove_first(iterator) if i > 0 else iterator)
 
-def save_doctor(row):
-    conn = psycopg2.connect(**conn_params)
-    cur = conn.cursor()
-    cur.execute("INSERT INTO doctor (name, last_name, clinical_specialization_id) VALUES (%s, %s, %s) RETURNING id", (row[5], row[6], row[7]))
-    doctor_id = cur.fetchone()[0]
-    conn.commit()
-    cur.close()
-    conn.close()
-    return (doctor_id, row)
+print('CSV file content without header')
+rows_rdd.foreach(lambda row: print(row))
 
-def save_appointment(row):
-    conn = psycopg2.connect(**conn_params)
-    cur = conn.cursor()
-    cur.execute("INSERT INTO appointment (date, time, patient_id, doctor_id) VALUES (%s, %s, %s, %s)", (row[3], row[4], row[1], row[0]))
-    conn.commit()
-    cur.close()
-    conn.close()
-    return row
+# Obtaining column values of the CSV by splitting by ','
+mapped_rdd = rows_rdd.map(lambda row: row.split(','))
 
-# Transform data using RDDs
-transformed_data = data.map(split_row)\
-    .map(save_patient)\
-    .map(save_clinical_specialization)\
-    .map(save_doctor)\
-    .map(save_appointment)
+print('CSV file content')
+mapped_rdd.foreach(lambda row: print(row))
 
-# Collect results and print to console
-results = transformed_data.collect()
-print(results)
+# Obtaining individual RDDs for each table
+patient_rdd = mapped_rdd.map(lambda row: (row[0], row[1], row[3]))
+clinical_specialization_rdd = mapped_rdd.map(lambda row: (row[7]))
+doctor_rdd = mapped_rdd.map(lambda row: (row[5], row[6]))
+appointment_rdd = mapped_rdd.map(lambda row: (row[3], row[4]))
+
+# Saving to postgres
+print('Saving to postgres...')
+df1 = patient_rdd.toDF(['name', 'last_name', 'address'])
+save_to_postgres(df1, 'patient')
+
+print('Saved to postgres complete!')
